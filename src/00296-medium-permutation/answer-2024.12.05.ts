b0cb89e93b53b16f4c2c3f4f78ecbd35aa659656
zhangpaopao <zhangpaopao0609@gmail.com>
2023-12-07 11:11:05 +0800

    feat: 00296-medium-permutation

--- src/00296-medium-permutation/answer-2024.12.05.ts
+++ src/00296-medium-permutation/answer-2024.12.05.ts
@@ -16,7 +16,14 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Permutation<T> = any
+type Permutation<T, K=T> =
+    [T] extends [never]
+      ? []
+      : K extends K
+        ? [K, ...Permutation<Exclude<T, K>>]
+        : never
+
+type Permuted = Permutation<'a' | 'b'>  // ['a', 'b'] | ['b' | 'a']
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
