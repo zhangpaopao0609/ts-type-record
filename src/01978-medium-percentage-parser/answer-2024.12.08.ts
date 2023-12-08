809323ec35733a7a2e23c03481e8596b3617faf0
zhangpaopao <zhangpaopao0609@gmail.com>
2023-12-08 11:43:50 +0800

    feat: 02070-medium-drop-char

--- src/01978-medium-percentage-parser/answer-2024.12.08.ts
+++ src/01978-medium-percentage-parser/answer-2024.12.08.ts
@@ -55,9 +55,6 @@
 //     : [F, ...CheckSuffix<R>]
 //   : ['', '', '']
 
-
-
-
 type CheckPrefix<T> = T extends '+' | '-' ? T : never;
 type CheckSuffix<T> =  T extends `${infer P}%` ? [P, '%'] : [T, ''];
 type PercentageParser<A extends string> = A extends `${CheckPrefix<infer L>}${infer R}` ? [L, ...CheckSuffix<R>] : ['', ...CheckSuffix<A>];
