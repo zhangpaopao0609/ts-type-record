7267f8ee2e6e5067b9874c473000f14f384ff28d
zhangpaopao <zhangpaopao0609@gmail.com>
2023-12-12 23:01:30 +0800

    feat: 29785-medium-deep-omit

---
+++ src/29785-medium-deep-omit/answer-2024.12.12.ts
@@ -0,0 +1,78 @@
+/*
+  29785 - Deep Omit
+  -------
+  by bowen (@jiaowoxiaobala) #medium #omit object-keys deep
+
+  ### Question
+
+  Implement a type`DeepOmit`, Like Utility types [Omit](https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys), A type takes two arguments.
+
+  For example:
+
+  ```ts
+  type obj = {
+    person: {
+      name: string;
+      age: {
+        value: number
+      }
+    }
+  }
+
+  type test1 = DeepOmit<obj, 'person'>    // {}
+  type test2 = DeepOmit<obj, 'person.name'> // { person: { age: { value: number } } }
+  type test3 = DeepOmit<obj, 'name'> // { person: { name: string; age: { value: number } } }
+  type test4 = DeepOmit<obj, 'person.age.value'> // { person: { name: string; age: {} } }
+  ```
+
+  > View on GitHub: https://tsch.js.org/29785
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type DeepOmit<T extends obj, U extends string> =
+  U extends `${infer F}.${infer R}`
+  ? { 
+      [K in keyof T]: 
+        K extends F
+        ? T[K] extends obj 
+          ? DeepOmit<T[K], R> 
+          : T[K]
+        : T[K]
+    }
+  : { [K in keyof T as K extends U ? never : K ]: T[K] }
+
+// type DeepOmit<T, U> = {
+//   [K in keyof T as  K extends U ? never : K]:
+//     U extends `${infer F}.${infer R}`
+//     ? K extends F
+//       ? DeepOmit<T[K], R>
+//       :T[K]
+//     :T[K]
+// }
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type obj = {
+  person: {
+    name: string
+    age: {
+      value: number
+    }
+  }
+}
+type cc = DeepOmit<obj, 'person.name'>
+type cases = [
+  Expect<Equal<DeepOmit<obj, 'person'>, {}>>,
+  Expect<Equal<DeepOmit<obj, 'person.name'>, { person: { age: { value: number } } }>>,
+  Expect<Equal<DeepOmit<obj, 'name'>, obj>>,
+  Expect<Equal<DeepOmit<obj, 'person.age.value'>, { person: { name: string; age: {} } }>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/29785/answer
+  > View solutions: https://tsch.js.org/29785/solutions
+  > More Challenges: https://tsch.js.org
+*/
