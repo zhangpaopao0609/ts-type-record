6eb23bcf75029148d8af46ae86b704b9a09ef52d
zhangpaopao <zhangpaopao0609@gmail.com>
2023-12-12 23:21:22 +0800

    feat: 30430-medium-tower-of-hanoi

---
+++ src/30430-medium-tower-of-hanoi/answer-2024.12.12.ts
@@ -0,0 +1,44 @@
+/*
+  30430 - Tower of hanoi
+  -------
+  by null (@aswinsvijay) #medium #array
+
+  ### Question
+
+  Simulate the solution for the Tower of Hanoi puzzle. Your type should take the number of rings as input an return an array of steps to move the rings from tower A to tower B using tower C as additional. Each entry in the array should be a pair of strings `[From, To]` which denotes ring being moved `From -> To`.
+
+  [Wikipedia](https://en.wikipedia.org/wiki/Tower_of_Hanoi)
+  [GeeksForGeeks](https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi)
+
+  > View on GitHub: https://tsch.js.org/30430
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Hanoi<
+  Rings extends number, 
+  FromRod extends string = 'A', 
+  ToRod extends string = 'B', 
+  IntermediateRod extends string = 'C', 
+  Acc extends '🇺🇦'[] = []
+> = Acc['length'] extends Rings 
+  ? [] 
+  : [ ...Hanoi<Rings, FromRod, IntermediateRod, ToRod, [...Acc, '🇺🇦']>, [FromRod, ToRod], ...Hanoi<Rings, IntermediateRod, ToRod, FromRod, [...Acc, '🇺🇦']> ]
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type Tests = [
+  Expect<Equal<Hanoi<0>, []>>,
+  Expect<Equal<Hanoi<1>, [['A', 'B']]>>,
+  Expect<Equal<Hanoi<2>, [['A', 'C'], ['A', 'B'], ['C', 'B']]>>,
+  Expect<Equal<Hanoi<3>, [['A', 'B'], ['A', 'C'], ['B', 'C'], ['A', 'B'], ['C', 'A'], ['C', 'B'], ['A', 'B']]>>,
+  Expect<Equal<Hanoi<5>, [['A', 'B'], ['A', 'C'], ['B', 'C'], ['A', 'B'], ['C', 'A'], ['C', 'B'], ['A', 'B'], ['A', 'C'], ['B', 'C'], ['B', 'A'], ['C', 'A'], ['B', 'C'], ['A', 'B'], ['A', 'C'], ['B', 'C'], ['A', 'B'], ['C', 'A'], ['C', 'B'], ['A', 'B'], ['C', 'A'], ['B', 'C'], ['B', 'A'], ['C', 'A'], ['C', 'B'], ['A', 'B'], ['A', 'C'], ['B', 'C'], ['A', 'B'], ['C', 'A'], ['C', 'B'], ['A', 'B']]>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/30430/answer
+  > View solutions: https://tsch.js.org/30430/solutions
+  > More Challenges: https://tsch.js.org
+*/
